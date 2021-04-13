a70f07261628d6b83495bf25557dd6aa3491dba1
xNebulaa <ah.nabilnasr@gmail.com>
2021-04-13 07:29:51 +0200

    upload image

--- src/app/components/draw/draw.component.ts
+++ src/app/components/draw/draw.component.ts
@@ -2,16 +2,13 @@ import { Component, OnInit, NgZone } from '@angular/core';
 import { AuthService } from "../../shared/services/auth.service";
 import { Router } from "@angular/router";
 import { fabric } from 'fabric';
-import { fromEvent, Observable, Subscription } from "rxjs";
-import { trigger } from '@angular/animations';
-import { Canvas } from 'fabric/fabric-impl';
 @Component({
   selector: 'app-draw',
   templateUrl: './draw.component.html',
   styleUrls: ['./draw.component.css']
 })
 export class DrawComponent implements OnInit {
-  canvas: any;
+  public canvas: any;
   color: any;
   brush: any;
   constructor(
@@ -21,10 +18,12 @@ export class DrawComponent implements OnInit {
   ) { }
   
   async ngOnInit(): Promise<any>{
+      const canvasSize= document.getElementsByClassName('canvasWrapper');
       this.canvas = new fabric.Canvas('canvas',{
         selectionLineWidth: 2,
-        width: 950,
-        height: 950,
+        width: canvasSize[0].clientWidth*0.9,
+        height: canvasSize[0].clientHeight*0.9,
+        backgroundColor: '#FFFF',
         isDrawingMode: true,
       });
     this.brush=this.canvas.freeDrawingBrush;
@@ -47,4 +46,31 @@ export class DrawComponent implements OnInit {
     this.canvas.clear();
     this.canvasModifiedCallback();
   }
+  onSelectFile(event) { // called each time file input changes
+    if (event.target.files && event.target.files[0]) {
+      const file = event.target.files[0];
+      const img =new Image();
+      img.src= URL.createObjectURL(file);
+      const useFromURL = () => {
+        //const canvas= this.canvas;
+        fabric.Image.fromURL(img.src, (img) => {
+          this.canvas.add(img);
+        },{
+          crossOrigin:'annonymous'
+        });
+        return this.canvas;
+      }
+      fabric.Image.prototype.toDatalessObject = fabric.Image.prototype.toObject;
+      fabric.Image.prototype.toObject = (function(toObject) {
+        return function() {
+          return fabric.util.object.extend(toObject.call(this), {
+            src: this.toDataURL()
+          });
+        };
+      })(fabric.Image.prototype.toObject);
+      
+      this.canvas = useFromURL();
+      console.log(this.canvas.toJSON);
+    }
+  };
 } 
